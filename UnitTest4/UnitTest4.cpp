f37745947ba6467fd852a9af4edd5b26558d8a58
viksi01 <victoria.pyatnicka@gmail.com>
2022-11-04 01:24:22 +0200

    master1_commit

---
+++ UnitTest4/UnitTest4.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_5.4/lab_5.4.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest4
+{
+	TEST_CLASS(UnitTest4)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double t;
+			t = S1(4, 1, 4);
+			Assert::AreEqual (t, 0.0);
+		}
+	};
+}
